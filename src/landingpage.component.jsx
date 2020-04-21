ce03e30d1fa692d5206ca4f93a58d5fdb4cb2f17
Dawit <dawit@ginetta.net>
2020-04-21 12:31:55 +0200

    Set up landingpage component with its main inside components

---
+++ src/landingpage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Landingpage = () => {
+    return (
+        <div className="landingpage">
+           <div className="dictionary-holder">
+               <div className="translate-from">
+                   <div className="translate-to"></div>
+               </div>
+           </div>
+        </div>
+    )
+}
