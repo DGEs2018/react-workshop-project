ecd12085af6cbc59cf629ed45573ac7cad798506
Dawit <dawit@ginetta.net>
2020-04-21 12:38:52 +0200

    Insert translate from subtitle inside the app

--- src/landingpage.component.jsx
+++ src/landingpage.component.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export const Landingpage = () => {
     return (
         <div className="landingpage">
+        <h2 className="translate-from">Translate from languagename</h2>
            <div className="dictionary-holder">
                <div className="translate-from">
                    <div className="translate-to"></div>
